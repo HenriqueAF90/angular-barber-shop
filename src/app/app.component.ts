5b59ce94e4182d39bed5c9cadfe9813454f7a5b9
Henrique Ferreira <henriqueandrade13@hotmail.com>
2023-09-23 09:28:32 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-barber_shop-landing_page';
+}
